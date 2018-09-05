9e0325f817f17d7185268e61d8aa157161a441f6
韦绍炜 <39393527+WiSiW@users.noreply.github.com>
2018-09-05 10:06:56 +0800

    Add files via upload
    未完成

--- linkGame.js
+++ linkGame.js
@@ -57,6 +57,37 @@ function isLink (first,second) {
     }
     // 同行
     if(startRow == endRow){
+        // 判断是否在边框
+        if(startRow == 0 || startRow == linkList[0].length-1){
+            // 判断是否
+            if(isDelete){
+                canDelete(first,second);
+            }
+            return;
+        }
+        // 在表格内
+        // 上方
+        for(let i=startLine;i<endLine+1;i++) {
+            if ($("#row" + startRow - 1).find("#line" + i).children().length > 0) {
+                isDelete = 0;
+            }
+            // 判断是否
+            if (isDelete) {
+                canDelete(first, second);
+                return;
+            }
+        }
+        // 下方
+        for(let i=startLine;i<endLine+1;i++){
+            if($("#row"+ startRow+1).find("#line"+ i).children().length > 0){
+                isDelete = 0;
+            }
+            // 判断是否
+            if(isDelete){
+                canDelete(first,second);
+                return;
+            }
+        }
         // 相邻
         if(startLine === endLine-1){
             canDelete(first,second)
@@ -66,10 +97,6 @@ function isLink (first,second) {
                     isDelete = 0;
                 }
             }
-            // 判断是否能通过表格外围相连
-            if(startRow == 0 || startRow == linkList[0].length){
-
-            }
             // 判断是否
             if(isDelete){
                 canDelete(first,second);
@@ -78,6 +105,39 @@ function isLink (first,second) {
     }
     // 同列
     if(startLine == endLine){
+        // 判断左右侧是否有方格
+        // 在边框
+        if(startLine == 0 || startLine == linkList[0].length-1){
+            // 判断是否
+            if(isDelete){
+                canDelete(first,second);
+                return;
+            }
+        }
+        // 在表格内
+        // 左侧
+        for(let i=startRow;i<endRow+1;i++) {
+            if ($("#row" + i).find("#line" + startLine - 1).children().length > 0) {
+                isDelete = 0;
+            }
+            // 判断是否
+            if (isDelete) {
+                canDelete(first, second);
+                return;
+            }
+        }
+        // 右侧
+        for(let i=startRow;i<endRow+1;i++){
+            if($("#row"+ i).find("#line"+ startLine+1).children().length > 0){
+                isDelete = 0;
+            }
+            // 判断是否
+            if(isDelete){
+                canDelete(first,second);
+                return;
+            }
+        }
+
         // 相邻
         if(startRow === endRow-1){
             canDelete(first,second)
@@ -88,10 +148,6 @@ function isLink (first,second) {
                     isDelete = 0;
                 }
             }
-            // 判断是否能通过表格外围相连
-            if(startLine == 0 || startLine == linkList[0].length){
-
-            }
             // 判断是否
             if(isDelete){
                 canDelete(first,second);
@@ -100,15 +156,60 @@ function isLink (first,second) {
     }
     // 不同行也不同列
     if(startRow != endRow && startLine != endLine){
-        // 判断中间是否有方格
-        for(let i=startRow-1;i<endRow;i++){
-            for(let j=startLine-1;j<endLine;j++){
-                if($("#row"+ i).find("#line"+ j).children().length > 0){
+        /**
+         * 在相邻行/列,但不为对角线
+         */
+        // 在相邻行
+        if(startRow == endRow-1){
+            for(let i = startLine+1;i<endLine+1;i++){
+                if($("#row"+ startRow).find("#line"+ i).children().length > 0){
                     isDelete = 0;
                 }
             }
+            // 判断是否
+            if(isDelete){
+                canDelete(first,second);
+                return;
+            }
+            for(let i = startLine;i<endLine;i++){
+                if($("#row"+ endRow).find("#line"+ i).children().length > 0){
+                    isDelete = 0;
+                }
+            }
+            // 判断是否
+            if(isDelete){
+                canDelete(first,second);
+                return;
+            }
         }
-        // 为对角线
+
+        // 在相邻列
+        if(startLine == endLine-1){
+            for(let i = startRow+1;i<endRow+1;i++){
+                if($("#row"+ i).find("#line"+ startLine).children().length > 0){
+                    isDelete = 0;
+                }
+            }
+            // 判断是否
+            if(isDelete){
+                canDelete(first,second);
+                return;
+            }
+            for(let i = startRow;i<endRow;i++){
+                if($("#row"+ i).find("#line"+ endLine).children().length > 0){
+                    isDelete = 0;
+                }
+            }
+            // 判断是否
+            if(isDelete){
+                canDelete(first,second);
+                return;
+            }
+        }
+
+        /**
+         * 为对角线
+         */
         if(startRow === endRow-1 && startLine === endLine-1){
             if($("#row"+ startRow).find("#line"+ startLine).children().length < 1){
                 isDelete = 1;
@@ -121,6 +222,91 @@ function isLink (first,second) {
             }
             if($("#row"+ endRow).find("#line"+ endLine).children().length < 1){
                 isDelete = 1;
+            }
+            // 判断是否
+            if(isDelete){
+                canDelete(first,second);
+                return;
+            }
+        }
+
+        /**
+         * 不在相邻行或列
+         */
+        /**
+         *  一个在边框
+         */
+        // 在左边框
+        //if(startLine === 0){
+        //    for(let i=startLine;i<endLine;i++){
+        //        if($("#row"+ startRow).find("#line"+ i).children().length > 0){
+        //            isDelete = 0;
+        //        }
+        //        if($("#row"+ endRow).find("#line"+ i).children().length > 0){
+        //            isDelete = 0;
+        //        }
+        //    }
+        //    // 判断是否
+        //    if(isDelete){
+        //        canDelete(first,second);
+        //        return;
+        //    }
+        //}
+        //// 在右边框
+        //if(endLine === linkList[0].length-1){
+        //    for(let i=startLine+1;i<endLine+1;i++){
+        //        if($("#row"+ startRow).find("#line"+ i).children().length > 0){
+        //            isDelete = 0;
+        //        }
+        //        if($("#row"+ endRow).find("#line"+ i).children().length > 0){
+        //            isDelete = 0;
+        //        }
+        //    }
+        //    // 判断是否
+        //    if(isDelete){
+        //        canDelete(first,second);
+        //        return;
+        //    }
+        //}
+        //// 在上边框
+        //if(startRow === 0){
+        //    for(let i=startRow;i<endRow;i++){
+        //        if($("#row"+ i).find("#line"+ startLine).children().length > 0){
+        //            isDelete = 0;
+        //        }
+        //        if($("#row"+ i).find("#line"+ endLine).children().length > 0){
+        //            isDelete = 0;
+        //        }
+        //    }
+        //    // 判断是否
+        //    if(isDelete){
+        //        canDelete(first,second);
+        //        return;
+        //    }
+        //}
+        //// 在下边框
+        //if(endRow === linkList.length-1){
+        //    for(let i=startRow+1;i<endRow+1;i++){
+        //        if($("#row"+ i).find("#line"+ startLine).children().length > 0){
+        //            isDelete = 0;
+        //        }
+        //        if($("#row"+ i).find("#line"+ endLine).children().length > 0){
+        //            isDelete = 0;
+        //        }
+        //    }
+        //    // 判断是否
+        //    if(isDelete){
+        //        canDelete(first,second);
+        //        return;
+        //    }
+        //}
+
+        // 不为对角线,判断中间是否有方格
+        for(let i=startRow-1;i<endRow;i++){
+            for(let j=startLine-1;j<endLine;j++){
+                if($("#row"+ i).find("#line"+ j).children().length > 0){
+                    isDelete = 0;
+                }
             }
         }
         // 判断是否
